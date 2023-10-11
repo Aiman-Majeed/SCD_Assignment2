1e83bcd7136f8b0ada0f8484217621e55a21aca0
Aiman Majeed <l201272@lhr.nu.edu.pk>
2023-10-12 03:19:54 +0500

    generic class created

--- Main.java
+++ Main.java
@@ -1,4 +1,25 @@
+import java.util.*;
 
+class GenericStack<T>
+{
+    public Stack<T> stack = new Stack<>();
+    public void push(T item)
+    {
+        stack.push(item);
+    }
+    public T pop()
+    {
+        return stack.pop();
+    }
+    public boolean isEmpty()
+    {
+        return stack.isEmpty();
+    }
+    public int size()
+    {
+        return stack.size();
+    }
+}
 public class Main 
 {
     public static void main(String[] args) 
