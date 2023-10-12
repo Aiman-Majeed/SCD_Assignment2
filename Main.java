642a02241a5574e2cca8d3a295bdbef1af055a41
Aiman Majeed <l201272@lhr.nu.edu.pk>
2023-10-12 20:02:36 +0500

    Enter Stack Values and Size

--- Main.java
+++ Main.java
@@ -28,6 +28,24 @@ public class Main
 {
     public static void main(String[] args) 
     {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter Stack Size: ");
+        int size = s.nextInt();
+        GenericStack<Integer> intS = new GenericStack<>();
+        GenericStack<String> strS = new GenericStack<>();
 
+        System.out.println("Enter Stack Values");
+        for(int i = 0; i < size; i++)
+        {
+            if(s.hasNextInt())
+            {
+                intS.push(s.nextInt());
+            }
+            else
+            {
+                String in = s.next();
+                strS.push(in);
+            }
+        }
     }
 }
