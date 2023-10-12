8047c47c2179dfe7cc694a087e8e8ce1c18594b9
Aiman Majeed <l201272@lhr.nu.edu.pk>
2023-10-12 23:39:19 +0500

    MergeIntervals

--- Question3.java
+++ Question3.java
@@ -1,15 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
 
-class Interval 
+public class Question3
 {
-    int start;
-    int end;
+    public static class Interval 
+    {
+        int start;
+        int end;
 
-    public Interval(int s, int e)
-     {
-        start = s;
-        end = e;
+        public Interval(int s, int e)
+        {
+            start = s;
+            end = e;
+        }
     }
-}
-public class Question3 {
-    
+    public static void mergeIntervals(List<Interval> intervals)
+    {
+        if (intervals.isEmpty()) 
+        {
+            return;
+        }
+
+        int j = 0;
+        for (int i = 1; i < intervals.size(); i++) 
+        {
+            Interval curr = intervals.get(i);
+            Interval prev = intervals.get(j);
+
+            if (curr.start <= prev.end) 
+            {
+                prev.end = curr.end > prev.end ? curr.end : prev.end;
+            }
+            else
+            {
+                j++;
+                intervals.set(j, curr);
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        List<Interval> intervals = new ArrayList<>();
+        intervals.add(new Interval(1, 3));
+        intervals.add(new Interval(2, 6));
+        intervals.add(new Interval(8, 10));
+        intervals.add(new Interval(15, 18));
+
+        mergeIntervals(intervals);
+
+        for (Interval interval : intervals) 
+        {
+            System.out.println("[" + interval.start + ", " + interval.end + "]");
+        }
+    }
 }
